f26e4072bfd5bd06c8e43ac8f5103ce2354b3981
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-05-29 19:03:48 +0300

    Fix vk user db save

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,11 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_vkontakte_oauth access_token
-    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+    user = User.where(:url => access_token.info.urls.Vkontakte).first
+    if user
       user
     else
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'<hh user=vk>.com', :password => Devise.friendly_token[0,20])
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'@vk.com', :password => Devise.friendly_token[0,20])
     end
   end
 end
