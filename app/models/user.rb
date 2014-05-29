3a60f520dad8f3a5812f36c1215f5e46a7659a98
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-05-29 19:27:46 +0300

    debug user saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
       user
     else
       nu = User.new(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => "#{access_token.extra.raw_info.domain}@vk.com", :password => Devise.friendly_token[0,20])
-      nu.save!
+      nu.save
     end
   end
 end
