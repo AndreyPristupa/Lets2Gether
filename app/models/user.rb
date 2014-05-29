59eb59a18b57bd325c989d5c26307ea25b84d2ee
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-05-29 19:13:18 +0300

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   include TheComments::User
   has_many :projects
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
