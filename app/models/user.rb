79bb46fa7682f9d55519be6e412599ebed126066
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-05-29 15:16:30 +0300

    Fix vk authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,12 @@ class User < ActiveRecord::Base
   def comments_moderator? comment
     id == comment.holder_id
   end
+
+  def self.find_for_vkontakte_oauth access_token
+    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+      user
+    else
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'<hh user=vk>.com', :password => Devise.friendly_token[0,20])
+    end
+  end
 end
