1b272fa38f3980d6c0f2b4255e415c248eb19adf
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-05-30 09:41:11 +0300

    Fix saving photo_200_orig in DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,20 +37,20 @@ class User < ActiveRecord::Base
       #     expires=true expires_at=1401515103
       #     token="f6bae4a049486b543aed746b0989b84ebc0f9752cec94ea7c2915e3f6d414735a608a347e4e7cfda04b5a3566ec0d">
       # extra=#<OmniAuth::AuthHash
-      # raw_info=#<OmniAuth::AuthHash
-      #     bdate="6.3.1990"
-      #     city=314
-      #     country=2
-      #     first_name="Андрюша"
-      #     id=182319070
-      #     last_name="Уже-Не-Торт"
-      #     nickname=""
-      #     online=1
-      #     photo_100="http://cs620224.vk.me/v620224070/5c61/HxB--Qp9AB0.jpg"
-      #     photo_200_orig="http://cs620224.vk.me/v620224070/5c5e/L02whK7ynes.jpg"
-      #     photo_50="http://cs620224.vk.me/v620224070/5c62/zis60bx4gAk.jpg"
-      #     screen_name="kievcookie"
-      #     sex=2>>
+      #   raw_info=#<OmniAuth::AuthHash
+      #       bdate="6.3.1990"
+      #       city=314
+      #       country=2
+      #       first_name="Андрюша"
+      #       id=182319070
+      #       last_name="Уже-Не-Торт"
+      #       nickname=""
+      #       online=1
+      #       photo_100="http://cs620224.vk.me/v620224070/5c61/HxB--Qp9AB0.jpg"
+      #       photo_200_orig="http://cs620224.vk.me/v620224070/5c5e/L02whK7ynes.jpg"
+      #       photo_50="http://cs620224.vk.me/v620224070/5c62/zis60bx4gAk.jpg"
+      #       screen_name="kievcookie"
+      #       sex=2>>
       # info=#<OmniAuth::AuthHash::InfoHash
       #     first_name="Андрюша"
       #     image="http://cs620224.vk.me/v620224070/5c62/zis60bx4gAk.jpg"
@@ -68,7 +68,7 @@ class User < ActiveRecord::Base
 
       User.create!(
                    :provider  => access_token.provider,
-                   :image     => access_token.raw_info.photo_200_orig,
+                   :image     => access_token.extra.raw_info.photo_200_orig,
                    :firstname => access_token.info.first_name,
                    :nickname  => access_token.info.name,
                    :lastanme  => access_token.info.last_name,
