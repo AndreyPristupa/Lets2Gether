b368ce1f2197e397d9b1fc7ef0747097828ef05b
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-06-02 18:23:56 +0300

    Fix fog for paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,4 +10,4 @@ end
 Paperclip::Attachment.default_options[:storage] = :fog
 Paperclip::Attachment.default_options[:fog_credentials] = {:provider => "Local", :local_root => "#{Rails.root}/public"}
 Paperclip::Attachment.default_options[:fog_directory] = ""
-Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
+# Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
